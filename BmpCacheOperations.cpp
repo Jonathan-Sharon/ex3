6f6eaa4bd6c2ef104ea4ce33ae72fe8bc4103bec
Ron Zivlin <ron@zivlin.net>
2020-08-31 22:26:03 +0300

    added constructors

--- BmpCacheOperations.cpp
+++ BmpCacheOperations.cpp
@@ -1,8 +1,18 @@
 #include "CacheOperations.hpp"
 #include "Bmp.hpp"
 #include "file_reading.hpp"
+#include "Cache/CacheManagement.hpp"
 
-BmpConvertToGrayScale::BmpConvertToGrayScale(const std::string &imagePath) : m_bmp(imagePath){}
+BmpConvertToGrayScale::BmpConvertToGrayScale(const std::string &imagePath) : m_bmp(imagePath){
+
+    CacheManagement cacheManager;
+    BinaryStringHash HashOperation(imagePath);
+    
+    cacheManager.operate(HashOperation, "cache/tmp");
+    m_bmpHash = stoi(readFileContent("cache/tmp"));
+
+    remove("cache/tmp");
+}
 
 std::string BmpConvertToGrayScale::operate(){
     
@@ -10,7 +20,16 @@ std::string BmpConvertToGrayScale::operate(){
     return m_bmp.bmpWrite();
 }
 
-BmpRotate::BmpRotate(const std::string &imagePath) : m_bmp(imagePath){}
+BmpRotate::BmpRotate(const std::string &imagePath) : m_bmp(imagePath){
+
+    CacheManagement cacheManager;
+    BinaryStringHash HashOperation(imagePath);
+    
+    cacheManager.operate(HashOperation, "cache/tmp");
+    m_bmpHash = stoi(readFileContent("cache/tmp"));
+
+    remove("cache/tmp");
+}
 
 std::string BmpRotate::operate(){
     
