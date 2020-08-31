b341d40632fea8dc55579619dcb2de0d91d68d8d
Jonathan Sharon <sharonzahavisharon@gmail.com>
2020-08-31 05:59:47 +0000

    cache management fields updated && another method added

--- Cache/CacheManagement.hpp
+++ Cache/CacheManagement.hpp
@@ -7,14 +7,16 @@ public:
     CacheManagement();
     void operate(int argc, const char **const argv);
     void clear();
-    int search(std::string &) const;
+    int search(const std::string &) const;
 
 private:
-    void deleteLine(const char *file_name, int n);
+    void deleteLine(int n);
     void createTheLine(int);
+    bool operationIsValid(int argc, const char **const argv) const;
 
+    const std::string m_filePath = "cache.txt";
     std::string m_fileContent;
     char **m_line;
     int m_argc;
-    const std::string m_fileName = "cache.txt";
+    int m_sizeOfCache;
 };
