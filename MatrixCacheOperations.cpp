c3b8730c34899c4484365be40203f74b1c94352a
Ron Zivlin <ron@zivlin.net>
2020-08-31 22:21:26 +0300

    added constructors

--- MatrixCacheOperations.cpp
+++ MatrixCacheOperations.cpp
@@ -1,7 +1,24 @@
 #include "CacheOperations.hpp"
 #include "Matrix.hpp"
+#include "Cache/CacheManagement.hpp"
+#include "file_reading.hpp"
 
-MatrixAddition::MatrixAddition(const std::string &matrix1, const std::string &matrix2) : m_matrix1(matrix1), m_matrix2(matrix2){}
+MatrixAddition::MatrixAddition(const std::string &matrix1, const std::string &matrix2) : m_matrix1(matrix1), m_matrix2(matrix2){
+
+    CacheManagement cacheManager;
+
+    BinaryStringHash matrix1HashOperation(matrix1);
+    BinaryStringHash matrix2HashOperation(matrix2);
+    
+    cacheManager.operate(matrix1HashOperation, "cache/tmp");
+
+    m_matrix1Hash = stoi(readFileContent("cache/tmp"));
+    cacheManager.operate(matrix2HashOperation, "cache/tmp");
+
+    m_matrix2Hash = stoi(readFileContent("cache/tmp"));
+
+    remove("cache/tmp");
+}
 
 std::string MatrixAddition::operate() const{
 
@@ -14,7 +31,22 @@ std::string MatrixAddition::getInfo() const{
 }
 
 
-MatrixMultipication::MatrixMultipication(const std::string &matrix1, const std::string &matrix2) : m_matrix1(matrix1), m_matrix2(matrix2){}
+MatrixMultipication::MatrixMultipication(const std::string &matrix1, const std::string &matrix2) : m_matrix1(matrix1), m_matrix2(matrix2){
+
+    CacheManagement cacheManager;
+
+    BinaryStringHash matrix1HashOperation(matrix1);
+    BinaryStringHash matrix2HashOperation(matrix2);
+    
+    cacheManager.operate(matrix1HashOperation, "cache/tmp");
+
+    m_matrix1Hash = stoi(readFileContent("cache/tmp"));
+    cacheManager.operate(matrix2HashOperation, "cache/tmp");
+
+    m_matrix2Hash = stoi(readFileContent("cache/tmp"));
+
+    remove("cache/tmp");
+}
 
 std::string MatrixMultipication::operate() const{
 
